adfa68ec7fde4d76fb6e85119966cfa80ce62832
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-09-13 04:21:01 -0300

    Commit inicial

---
+++ entrega-1-cardozo-adrian/Entities/Player.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+export (float) var speed: float = 300
+
+func _process(delta):
+	var direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	position.x += direction * speed * delta
